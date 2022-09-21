80d0c7457a5f8a1744d56750d926328bdef23199
rodrrigodev <rodrigosanyy@gmail.com>
2022-09-21 10:18:32 -0300

    comment form completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { Header } from "./components/Header";
-import { Post } from "./Post";
+import { Sidebar } from "./components/Sidebar";
+import { Post } from "./components/Post";
 
 import "./global.css";
 import styles from "./App.module.css";
-import { Sidebar } from "./components/Sidebar";
 
 export function App() {
   return (
